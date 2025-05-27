2ef5d5519b02c0a8e6c32bb92684ba754a39a691
Mattergx0 <Liamjurgens13@gmail.com>
2025-05-27 20:41:36 +0200

    Update and rename app.js to FolderItem.cs

---
+++ FolderItem.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace FileTaggerApp.Models
+{
+    public class FolderItem
+    {
+        public string Id { get; set; }  // Uniek id, bv. GUID
+        public string Name { get; set; }
+    }
+}
